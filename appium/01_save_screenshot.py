ee4913701c077d3094536f3d8aa5a1f61c70f9ba
weilili <540224203@qq.com>
2020-04-29 18:57:10 +0800

    appium第三次提交

---
+++ appium/01_save_screenshot.py
@@ -0,0 +1,31 @@
+from appium import webdriver
+from time import sleep
+
+# caps = {
+#     "platformName": "Android",
+#     "platformVersion": "7.0",
+#     "deviceName": "06157df6ad37300f",
+#     "appPackage": "com.lqr.wechat",
+#     "appActivity": "com.lqr.wechat.ui.activity.SplashActivity"
+# }
+
+caps = {
+    "platformName": "Android",
+    "platformVersion": "5.1.1",
+    "deviceName": "127.0.0.1:62001",
+    "appPackage": "com.lqr.wechat",
+    "appActivity": "com.lqr.wechat.ui.activity.SplashActivity"
+}
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+
+driver.find_element_by_id("com.lqr.wechat:id/btnLogin").click()
+sleep(1)
+driver.find_element_by_xpath("//*[@text='你的手机号码']").send_keys("18010181267")
+driver.find_element_by_id("com.lqr.wechat:id/etPwd").send_keys("123456")
+driver.find_element_by_xpath("//*[@text='登录']").click()
+sleep(2)
+
+address_list = driver.find_element_by_id("com.lqr.wechat:id/llContacts")
+
+driver.save_screenshot(r"C:\Users\Administrator\Desktop\123.png")
